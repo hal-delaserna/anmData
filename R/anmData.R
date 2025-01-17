95144af48438ee42ddbd8885ce1ba86b7b2d3458
Humberto Almeida de La Serna <humberto.serna@anm.gov.br>
2025-01-16 21:43:21 -0300

    reformulação ampla. Várias mudanças

--- R/anmData.R
+++ R/anmData.R
@@ -1,49 +1,43 @@
 # R/anmData.R
 
-#' Fetch ANM Data
+#' anmData
 #'
 #' This function fetches data from the ANM open data portal.
 #'
 #' @param code A character vector with the series code.
 #' @return A data frame containing the requested data.
+#' @importFrom utils read.table
+#' @importFrom utils data
 #' @export
 
 anmData <- function(code) {
 
-  url <- "https://app.anm.gov.br/DadosAbertos/"
-  read.table(
-    file = paste0(url, code),
-    header = TRUE,
-    sep = ",",
-    fill = TRUE,
-    stringsAsFactors = FALSE,
-    fileEncoding = "Latin1",
-    quote = "\""
+
+  if (!code %in% rownames(anm_paths)) {
+    stop("Invalid code! See available codes with `rownames(anm_paths)`.")
+  }
+
+
+  url_base <- "https://app.anm.gov.br/DadosAbertos"
+  path <- anm_paths[code, 1]
+  url <- paste0(url_base, path, ".csv")
+
+
+  tryCatch(
+    {
+      data <- read.table(
+        file = url,
+        header = TRUE,
+        sep = ",",
+        fill = TRUE,
+        stringsAsFactors = FALSE,
+        fileEncoding = "Latin1",
+        quote = "\""
+      )
+      return(data)
+    },
+    error = function(e) {
+      stop("Error downloading data. Check your connection or the code provided.")
+    }
   )
-
 }
-
-
-# fetch_anm_data <- function(endpoint) {
-#   base_url <- "https://dados.gov.br/dataset/anm-dados-abertos"
-#   url <- paste0(base_url, endpoint)
-#
-#   response <- httr::GET(url)
-#
-#   if (response$status_code == 200) {
-#     data <- jsonlite::fromJSON(content(response, "text"))
-#     return(dplyr::as_tibble(data))
-#   } else {
-#     stop("Failed to fetch data from ANM. Status code: ", response$status_code)
-#   }
-# }
-
-
-
-
-
-
-
-
-
-
